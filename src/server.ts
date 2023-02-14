7d2a79fb8428ddb074f8850c3a0d20a9341af163
Placide IRANDORA <placideirandora@gmail.com>
2023-02-14 14:15:10 +0200

    feat: play the game optimally for a win or tie

--- src/server.ts
+++ src/server.ts
@@ -22,23 +22,86 @@ app.get("/", (req: Request, res: Response) => {
     }
   }
 
-  const firstPlayer = boardArray.find((cell) => cell !== " ");
+  const firstPlayer = boardArray.find((square) => square !== " ");
 
   if (xCount < oCount || (xCount === oCount && firstPlayer === "x")) {
     return res.status(400).send("Bad Request - Not o's turn");
   }
 
+  let moveMade = false;
+
+  // Check for a winning move
   for (let i = 0; i < boardArray.length; i++) {
     if (boardArray[i] === " ") {
       boardArray[i] = "o";
-      break;
+      if (checkForWin(boardArray, "o")) {
+        moveMade = true;
+        break;
+      }
+      boardArray[i] = " ";
+    }
+  }
+
+  // Check for a blocking move
+  if (!moveMade) {
+    for (let i = 0; i < boardArray.length; i++) {
+      if (boardArray[i] === " ") {
+        boardArray[i] = "x";
+        if (checkForWin(boardArray, "x")) {
+          boardArray[i] = "o";
+          moveMade = true;
+          break;
+        }
+        boardArray[i] = " ";
+      }
+    }
+  }
+
+  // Make any move if no other moves have been made
+  if (!moveMade) {
+    for (let i = 0; i < boardArray.length; i++) {
+      if (boardArray[i] === " ") {
+        boardArray[i] = "o";
+        moveMade = true;
+        break;
+      }
     }
   }
 
   return res.status(200).send(boardArray.join(""));
 });
 
-app.listen(3000, () => {
+app.listen(3000, (): void => {
   console.log("Server is listening on port 3000 ");
 });
 
+const checkForWin = (board: string[], player: string): boolean => {
+  const horizontalWinningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+  const verticalWinningPositions = [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+  const diagonalWinningPositions = [
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const winningPositions = [
+    ...horizontalWinningPositions,
+    ...verticalWinningPositions,
+    ...diagonalWinningPositions,
+  ];
+
+  for (const positions of winningPositions) {
+    if (positions.every((position) => board[position] === player)) {
+      return true;
+    }
+  }
+
+  return false;
+};
